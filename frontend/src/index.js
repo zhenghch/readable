f929ec8a0bb3a7ff695cfdec2542a0850826d8c6
Hancheng ZHENG <hanc.zheng@gmail.com>
2018-01-24 18:11:50 +0800

    remove initial files provided by createMyApp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import Actions from './actions';
+
+let store = createStore(reducer);
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch({ type: Actions.SELECT_CATEGORY, category: 'test'});
+store.dispatch({ type: Actions.SELECT_CATEGORY, category: 'all'});
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
